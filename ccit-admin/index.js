107becc351e8d82d6501e45bf2850c5d1b53c0b7
Cyber Connect IT <100221210+cyberconnectit@users.noreply.github.com>
2023-07-18 09:37:19 +0100

    Add files via upload

---
+++ ccit-admin/index.js
@@ -0,0 +1,115 @@
+const primaryColor = '#4834d4'
+const warningColor = '#f0932b'
+const successColor = '#6ab04c'
+const dangerColor = '#eb4d4b'
+
+const themeCookieName = 'theme'
+const themeDark = 'dark'
+const themeLight = 'light'
+
+const body = document.getElementsByTagName('body')[0]
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date()
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+  var expires = "expires="+d.toUTCString()
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"
+}
+
+function getCookie(cname) {
+  var name = cname + "="
+  var ca = document.cookie.split(';')
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ""
+}
+
+loadTheme()
+
+function loadTheme() {
+	var theme = getCookie(themeCookieName)
+	body.classList.add(theme === "" ? themeLight : theme)
+}
+
+function switchTheme() {
+	if (body.classList.contains(themeLight)) {
+		body.classList.remove(themeLight)
+		body.classList.add(themeDark)
+		setCookie(themeCookieName, themeDark)
+	} else {
+		body.classList.remove(themeDark)
+		body.classList.add(themeLight)
+		setCookie(themeCookieName, themeLight)
+	}
+}
+
+function collapseSidebar() {
+	body.classList.toggle('sidebar-expand')
+}
+
+window.onclick = function(event) {
+	openCloseDropdown(event)
+}
+
+function closeAllDropdown() {
+	var dropdowns = document.getElementsByClassName('dropdown-expand')
+	for (var i = 0; i < dropdowns.length; i++) {
+		dropdowns[i].classList.remove('dropdown-expand')
+	}
+}
+
+function openCloseDropdown(event) {
+	if (!event.target.matches('.dropdown-toggle')) {
+		// 
+		// Close dropdown when click out of dropdown menu
+		// 
+		closeAllDropdown()
+	} else {
+		var toggle = event.target.dataset.toggle
+		var content = document.getElementById(toggle)
+		if (content.classList.contains('dropdown-expand')) {
+			closeAllDropdown()
+		} else {
+			closeAllDropdown()
+			content.classList.add('dropdown-expand')
+		}
+	}
+}
+
+var ctx = document.getElementById('myChart')
+ctx.height = 500
+ctx.width = 500
+var data = {
+	labels: ['January', 'February', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	datasets: [{
+		fill: false,
+		label: 'completed',
+		borderColor: successColor,
+		data: [4, 3, 5, 2, 1, 5, 4, 2, 3, 1, 3],
+		borderWidth: 4,
+		lineTension: 0,
+	}, {
+		fill: false,
+		label: 'In Complete',
+		borderColor: dangerColor,
+		data: [1, 3, 5, 7, 2, 3, 4, 0, 6, 5, 3],
+		borderWidth: 4,
+		lineTension: 0,
+	}]
+}
+
+var lineChart = new Chart(ctx, {
+	type: 'bar',
+	data: data,
+	options: {
+		maintainAspectRatio: false,
+		bezierCurve: false,
+	}
+})
